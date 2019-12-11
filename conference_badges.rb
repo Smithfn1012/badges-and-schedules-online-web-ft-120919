3bdfa2f8a03e64b7685f09200955236e6d182442
Felicia Smith <smithfn1012@gmail.com>
2019-12-11 17:14:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,23 @@ def batch_badge_creator(array_of_names)
   end
 end
 
+def assign_rooms(array_of_names)
+  room_assignments = []
+  array_of_names.each_with_index do |name, index|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(attendee
+  attendee_badges = batch_badge_creator(attendees)
+  attendee_rooms = assign_rooms(attendees)
+
+  #test requires printer to puts attendee badges and room assignment messages one at a time
+  attendee_badges.each do |badge|
+    puts badge
+  end
+
+  attendee_rooms.each do |room_message|
+    puts room_message
+  end
+end
