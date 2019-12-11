3f8f392c4d24115f4106c1f8aec044820488e2b1
Felicia Smith <smithfn1012@gmail.com>
2019-12-11 17:10:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,7 @@
 def badge_maker(name)
-  
+  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  name.each do |speaker|
   puts "Hello, my name is #{name}."
+end
+return speakers
 end
